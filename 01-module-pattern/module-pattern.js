e4ebfabc11f36d97c4fda8e569b965b231868130
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-09-26 16:06:32 +0300

    Revealing Module Pattern

--- 01-module-pattern/module-pattern.js
+++ 01-module-pattern/module-pattern.js
@@ -14,10 +14,10 @@
 
 // Example 01: Standard Module Pattern
 const UICtrl = (function() {
-  let text = "Hello World";
+  let text = "Hello World: Standard Module Pattern";
 
   const changeText = function() {
-    const element = document.querySelector('h1');
+    const element = document.querySelector('.standard-module-pattern');
 
     element.textContent = text;
   }
@@ -31,3 +31,26 @@ const UICtrl = (function() {
 })();
 
 UICtrl.callChangeText();
+
+// Revealing Module Controller
+const ItemCtrl = (function() {
+  let data = [];
+
+  function add(item) {
+    data.push(item);
+    console.log('Item added...');
+  }
+
+  function get(id) {
+    return data.find(item => item.id == id);
+  }
+
+  return {
+    add,
+    get
+  }
+})();
+
+ItemCtrl.add({ id: 1, itemName: 'Item 1'});
+let element = document.querySelector('.revealing-module-pattern');
+element.textContent = `${ItemCtrl.get(1).itemName}: Revealing Module Pattern`;
